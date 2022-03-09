010bfce05ac2bfda4eb242e03ad43eec3dc9ee79
WhiteRose404 <eddoughri.youssef@gmail.com>
2022-03-09 01:42:38 +0100

    Add the run script for fast Compiling and the first version of the App
    the App is fully functional but there are some issue that can be avoided easly
    for instance the app will alaways wait 1 second before the event took place this
    can be fixed by using an cond variable, also use mutex to avoid race condition.

--- main.c
+++ main.c
@@ -2,8 +2,41 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <time.h>
+#include <unistd.h>
+
+int flag = 1, End = 1;
+// pthread_cond_t flagviewer;
+// pthread_mutex_t flagcontroler
+void* run(void* time){
+    int seconds = *(int *) time;
+    for(int i=seconds;i>0;i--){
+        while(!flag){
+            sleep(1);
+        }
+        int k = i;
+        int s = k%60; k = k/60;
+		int m = k%60; k = k/60;
+		int h = k%60; 
+ 	 	printf("%02d:%02d:%02d /*-*\\\n",h,m,s);
+		sleep(1);
+	}
+    End = 0;
+	return NULL;
+}
+
+
+void* listen(void* empty){
+    while(End){
+        scanf("%d",&flag);
+    }
+}
 
 int main(){
-    printf("all is well \n");
+    pthread_t clock,event;
+    int N = 3600;
+    pthread_create(&clock,NULL,&run,&N);
+    pthread_create(&event,NULL,&listen,NULL);
+    pthread_join(clock,NULL);
+    pthread_join(event,NULL);
     return 0;
 }
